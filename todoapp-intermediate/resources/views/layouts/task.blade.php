1b000202937294334e9a6b3d5fc9d73c3daff1ed
niiharamegumu <nhrmgm1223@gmail.com>
2019-02-24 17:20:13 +0900

    taskViewのレイアウト.

---
+++ todoapp-intermediate/resources/views/layouts/task.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Intermediate</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
