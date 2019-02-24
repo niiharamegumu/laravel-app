7ffc04cdd1fa83ab690ffc9aca888b0bf43e92b2
niiharamegumu <nhrmgm1223@gmail.com>
2019-02-24 17:12:14 +0900

    TaskController.

---
+++ todoapp-intermediate/app/Http/Controllers/TaskController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\TaskRepository;
+use App\Task;
+use Illuminate\Http\Request;
+
+
+class TaskController extends Controller
+{
+
+  public function __construct(TaskRepository $tasks) {
+    $this->middleware('auth');
+    $this->tasks = $tasks;
+  }
+
+    public function index ( Request $request ) {
+      return view('tasks.index', [
+        'tasks' => $this->tasks->forUser($request->user()),
+      ]);
+    }
+
+    public function add ( Request $request ) {
+      $this->validate($request, Task::$rules);
+      $request->user()->tasks()->create([
+        'name' => $request->name,
+      ]);
+      return redirect('/tasks');
+    }
+
+    public function destroy(Request $request, Task $task){
+      $this->authorize('destroy', $task);
+      $task->delete();
+      return redirect('/tasks');
+    }
+}
