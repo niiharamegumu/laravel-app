91794b3a90fca7614d8ab4739349c204486fc687
niiharamegumu <nhrmgm1223@gmail.com>
2019-02-26 00:28:28 +0900

    タスクの追加機能.

--- todo-advanced/routes/web.php
+++ todo-advanced/routes/web.php
@@ -11,8 +11,19 @@
 |
 */
 
+/**
+ * 各フォルダーのタスク表示
+*/
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
-
+/**
+ * フォルダーの新規追加
+*/
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+
+/**
+ * 各フォルダーにタスクの追加
+*/
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
