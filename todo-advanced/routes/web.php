109394e01c9092379b175073607d158ee3b6defd
niiharamegumu <nhrmgm1223@gmail.com>
2019-02-25 22:45:28 +0900

    Folder名の新規追加機能.

--- todo-advanced/routes/web.php
+++ todo-advanced/routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
