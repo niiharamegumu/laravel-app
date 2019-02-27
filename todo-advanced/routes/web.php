7e24364f00f5c79cfe191aef0f4841d5c3880f04
niiharamegumu <nhrmgm1223@gmail.com>
2019-02-28 02:25:28 +0900

    Auth認証の設定.Middleware.

--- todo-advanced/routes/web.php
+++ todo-advanced/routes/web.php
@@ -10,35 +10,39 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/**
- * TOP画面
-*/
-Route::get('/', 'HomeController@index')->name('home');
 
 /**
  * 認証機能
 */
 Auth::routes();
 
-/**
- * 各フォルダーのタスク表示
-*/
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
-/**
- * フォルダーの新規追加
-*/
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
+Route::group(['middleware' => 'auth'], function() {
+  /**
+  * TOP画面
+  */
+  Route::get('/', 'HomeController@index')->name('home');
 
-/**
- * 各フォルダーにタスクの追加
-*/
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+  /**
+  * 各フォルダーのタスク表示
+  */
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
-/**
- * タスクの編集
-*/
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+  /**
+  * フォルダーの新規追加
+  */
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
+
+  /**
+  * 各フォルダーにタスクの追加
+  */
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+  /**
+  * タスクの編集
+  */
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
