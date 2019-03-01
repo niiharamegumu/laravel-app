ab40ca74e674b8772d97b68919744a5be2afc7fb
niiharamegumu <nhrmgm1223@gmail.com>
2019-03-01 18:50:11 +0900

    Policy.403.権限外のアクセス対策.

--- todo-advanced/routes/web.php
+++ todo-advanced/routes/web.php
@@ -18,31 +18,21 @@ Auth::routes();
 
 
 Route::group(['middleware' => 'auth'], function() {
-  /**
-  * TOP画面
-  */
   Route::get('/', 'HomeController@index')->name('home');
-
-  /**
-  * 各フォルダーのタスク表示
-  */
-  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
-
-  /**
-  * フォルダーの新規追加
-  */
+  
   Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
   Route::post('/folders/create', 'FolderController@create');
 
-  /**
-  * 各フォルダーにタスクの追加
-  */
-  Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-  Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
 
-  /**
-  * タスクの編集
-  */
-  Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-  Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+  Route::group(['middleware' => 'can:view,folder'], function() {
+    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
+
+    Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+    Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+
+    Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+    Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+  });
+
+
 });
