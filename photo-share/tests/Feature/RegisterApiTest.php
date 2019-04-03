e5d42be49de38d1233d5f9797cada8bb96d419e3
niiharamegumu <nhrmgm1223@gmail.com>
2019-04-03 22:58:06 +0900

    Register機能のRegisterApiTest(PHPUnit)

---
+++ photo-share/tests/Feature/RegisterApiTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterApiTest extends TestCase
+{
+  use RefreshDatabase;
+  public function test_return_new_create_user() {
+    $data = [
+        'name' => 'vuesplash user',
+        'email' => 'dummy@email.com',
+        'password' => 'test1234',
+        'password_confirmation' => 'test1234',
+    ];
+
+    $response = $this->json('POST', route('register'), $data);
+
+    $user = User::first();
+    $this->assertEquals($data['name'], $user->name);
+
+    $response
+        ->assertStatus(201)
+        ->assertJson(['name' => $user->name]);
+    }
+}
